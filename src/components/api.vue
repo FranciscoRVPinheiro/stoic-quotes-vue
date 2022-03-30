f978fc28610875452810ad020ac365d5cd16b517
Ricardo <franciscorvpinheiro49@gmail.com>
2022-03-30 15:39:01 -0300

    add animation, new button

--- src/components/api.vue
+++ src/components/api.vue
@@ -5,13 +5,13 @@
 
           <!-- Quotes -->
         <div v-for="quote in quotes" :key="quote.id">
-          <p class="fst-italic fs-4">"{{ quote.quote }}"</p>
-          <h3 class="d-flex justify-content-center custom-font"><strong>{{ quote.author }}</strong></h3>
+          <p class="fst-italic fs-4 text-animation">"{{ quote.quote }}"</p>
+          <h3 class="d-flex justify-content-center custom-font text-animation">{{ quote.author }}</h3>
         </div>
 
           <!-- button -->
         <div class="d-flex justify-content-center p-5">
-          <button class="btn btn-lg btn-primary" @click="apiCall">Next</button>
+          <button class="btn btn-sample " @click="apiCall">Next</button>
         </div>
  
       </div>
@@ -49,6 +49,85 @@ apiCall()
 
 .custom-font {
   font-family: 'Hurricane', cursive;
+}
+
+.text-animation {
+
+    -webkit-animation: fadein 3s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein 3s; /* Firefox < 16 */
+         -o-animation: fadein 3s; /* Opera < 12.1 */
+            animation: fadein 3s;
+}
+
+@keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
+
+/* Firefox < 16 */
+@-moz-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
+
+/* Safari, Chrome and Opera > 12.1 */
+@-webkit-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
+
+/* Opera < 12.1 */
+@-o-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
+
+
+/* custom button */
+.btn-sample { 
+  color: #ffffff; 
+  background-color: #42B883; 
+  border-color: #44DB97; 
+} 
+ 
+.btn-sample:hover, 
+.btn-sample:focus, 
+.btn-sample:active, 
+.btn-sample.active, 
+.open .dropdown-toggle.btn-sample { 
+  color: #ffffff; 
+  background-color: #4CE6B8; 
+  border-color: #44DB97; 
+} 
+ 
+.btn-sample:active, 
+.btn-sample.active, 
+.open .dropdown-toggle.btn-sample { 
+  background-image: none; 
+} 
+ 
+.btn-sample.disabled, 
+.btn-sample[disabled], 
+fieldset[disabled] .btn-sample, 
+.btn-sample.disabled:hover, 
+.btn-sample[disabled]:hover, 
+fieldset[disabled] .btn-sample:hover, 
+.btn-sample.disabled:focus, 
+.btn-sample[disabled]:focus, 
+fieldset[disabled] .btn-sample:focus, 
+.btn-sample.disabled:active, 
+.btn-sample[disabled]:active, 
+fieldset[disabled] .btn-sample:active, 
+.btn-sample.disabled.active, 
+.btn-sample[disabled].active, 
+fieldset[disabled] .btn-sample.active { 
+  background-color: #42B883; 
+  border-color: #44DB97; 
+} 
+ 
+.btn-sample .badge { 
+  color: #42B883; 
+  background-color: #ffffff; 
 }
 
 </style>
