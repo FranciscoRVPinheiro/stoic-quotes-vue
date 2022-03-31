01983537f8d3a0eb56990ac6c3b981d6f4c82606
Ricardo <franciscorvpinheiro49@gmail.com>
2022-03-31 08:30:03 -0300

    fix favicon issue

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,12 @@ module.exports = {
   pwa: {
     name: "Stoic Quotes",
     themeColor: "#343a40",
+    iconPaths: {
+        favicon32: 'img/icons/stoics32.png',
+        favicon16: 'img/icons/stoics16.png',
+        appleTouchIcon: 'img/icons/apple152.png',
+        maskIcon: 'img/icons/safari-pinned-tab.svg',
+        msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
   } 
 };
